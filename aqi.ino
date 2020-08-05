f26497e8cb968a3d33c65dd898e0b0d730f7f277
andrew040 <info@alphapapa.nl>
2020-08-05 21:15:51 +0200

    Replaced Adafruit BME680 library with Bosch BSEC library. Changes to compiler and linker settings required when using BSEC.

--- aqi.ino
+++ aqi.ino
@@ -5,11 +5,11 @@
 //+ bme680 lib       289408 bytes
 //+ tsl2516 lib      289688 bytes
 //+ HTTP code        296220 bytes
+//+ BSEC lib         332525 bytes
 
 
 //TODO
 //Blink value over threshold
-//Implement BSEC library
 
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
@@ -21,7 +21,7 @@
 #include "ssd1306.h"
 #include "aqi.h"
 #include "nano_gfx.h"
-#include "Adafruit_BME680.h"
+#include "bsec.h"
 
 
 // Delay constants
@@ -36,7 +36,7 @@ const int DoneOffset = 98;
 const int maxX=128,maxY=63;
 const int height=10;
 const int margin=1;
-const int startY=53; //63-10-2=51
+const int startY=53; 
 
 // Functional status
 bool status = false;
@@ -45,22 +45,20 @@ long StartEpochTime, CurrentEpochTime;
 float LuxFloat;
 int j=0,k;
 
-
-
 // Type conversion variables
-String TimeString,IPString,AqiString,PlaintextString;
+String TimeString,IPString,AqiString,PlaintextString,output;
 char TimeChars[22],IPChars[22],AqiChars[22];
 char TemperatureChars[22], PressureChars[22], HumidityChars[22];
 char LuxChars[22];
 
 // Initialize objects
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org",0, 60000);
 WiFiClient connection;
-Adafruit_BME680 SensorBME; // I2C
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org",0, 60000);
 IPAddress ip;
 Adafruit_TSL2561_Unified SensorTSL = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 sensors_event_t event;
+Bsec iaqSensor;
 
 // Convert IPAddress object to string
 String IpAddress2String(const IPAddress& ipAddress)
@@ -71,20 +69,41 @@ String IpAddress2String(const IPAddress& ipAddress)
   String(ipAddress[3])  ;
 }
 
+void checkIaqSensorStatus(void)
+{
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      Serial.println("BSEC error code : " + String(iaqSensor.status));
+      while(1);
+    } else {
+      Serial.println("BSEC warning code : " + String(iaqSensor.status));
+    }
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      Serial.println("BME680 error code : " + String(iaqSensor.bme680Status));
+      while(1);
+    } else {
+      Serial.println("BME680 warning code : " + String(iaqSensor.bme680Status));
+    }
+  }
+}
+
 // Draw graphics on display
 static void drawText()
 {
   int GraphRange = maxX-2*margin;
-  float aqi = 12.3;
   int drawPercent;
 
   if(LuxFloat > 4){
       //blinky
       status = status ? false : true;
 
-      snprintf(TemperatureChars, sizeof TemperatureChars, "Temperature: %.2f C", SensorBME.temperature);
-      snprintf(PressureChars, sizeof PressureChars, "Pressure: %.1f hPa", SensorBME.pressure / 100.0);
-      snprintf(HumidityChars, sizeof HumidityChars, "Humidity: %.1f %%", SensorBME.humidity);
+      snprintf(TemperatureChars, sizeof TemperatureChars, "Temperature: %.2f C", iaqSensor.temperature);
+      snprintf(PressureChars, sizeof PressureChars, "Pressure: %.1f hPa", iaqSensor.pressure / 100.0);
+      snprintf(HumidityChars, sizeof HumidityChars, "Humidity: %.1f %%", iaqSensor.humidity);
+      snprintf(AqiChars, sizeof AqiChars, "AQI: %.1f    ", iaqSensor.iaq);
       snprintf(LuxChars, sizeof LuxChars, "Light: %4.0f lux ", LuxFloat);
       
       ssd1306_positiveMode();
@@ -104,12 +123,7 @@ static void drawText()
       ssd1306_printFixed(0,  40, "VOC's: 120 ppb", STYLE_NORMAL);
       ssd1306_positiveMode();
       */
-    
-      //Air quality part:
-      AqiString = "AQI: ";
-      AqiString = AqiString + aqi;
-      strcpy(AqiChars, AqiString.c_str());
-    
+
       //Draw bar outline
       ssd1306_printFixed(0,40, AqiChars, STYLE_NORMAL);
       ssd1306_drawLine(margin,startY,maxX-margin,startY);
@@ -124,7 +138,7 @@ static void drawText()
       ssd1306_drawVLine(101,startY,startY+height);
       
     
-      drawPercent = aqi/500*GraphRange;
+      drawPercent = iaqSensor.iaq/500*GraphRange;
       ssd1306_fillRect(margin,startY,margin+drawPercent,startY+height);
   } else {
       ssd1306_clearScreen();  
@@ -186,16 +200,26 @@ void setup()
 
   //Initialize BME680 sensor
   ssd1306_printFixed(0,  32, "Init BME680...", STYLE_NORMAL);
-  if (!SensorBME.begin()) {
-    Serial.println("No BME680 detected. Rebooting.");
-    ssd1306_printFixed(DoneOffset,  32, "FAIL!", STYLE_NORMAL);
-  }
+  
+  iaqSensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
+  checkIaqSensorStatus();
 
-  SensorBME.setTemperatureOversampling(BME680_OS_8X);
-  SensorBME.setHumidityOversampling(BME680_OS_2X);
-  SensorBME.setPressureOversampling(BME680_OS_4X);
-  SensorBME.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  SensorBME.setGasHeater(320, 150); // 320*C for 150 ms
+  // BME sensor values
+  bsec_virtual_sensor_t sensorList[10] = {
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+    BSEC_OUTPUT_RAW_GAS,
+    BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_STATIC_IAQ,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+  };  
+  
+  iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+
   ssd1306_printFixed(DoneOffset,  32, "Done!", STYLE_NORMAL);
   
   //Initialize TSL2561 sensor
@@ -228,31 +252,10 @@ void loop()
   timeClient.update();
   TimeString = timeClient.getFormattedTime();
   TimeString = TimeString.substring(0,5);
-  TimeString = "Time: " + TimeString + "     UTC+2";
+  TimeString = "Time: " + TimeString + "UTC";
   strcpy(TimeChars, TimeString.c_str());
-
-  // Read BME
-  if (! SensorBME.performReading()) {
-    Serial.println("Reading BME680 failed.");
-    return;
-  }
-
-  // Read TSL
-  SensorTSL.getEvent(&event);
-  if (event.light) LuxFloat = event.light;
-
-
-  //Convert measurements to string
-  PlaintextString = "S-";
-  PlaintextString = PlaintextString + String(timeClient.getEpochTime())+"-";
-  PlaintextString = PlaintextString + String(SensorBME.temperature)+"-";
-  PlaintextString = PlaintextString + String(SensorBME.pressure / 100.0)+"-";
-  PlaintextString = PlaintextString + String(SensorBME.humidity)+"-";
-  PlaintextString = PlaintextString + String(LuxFloat)+"-";
-  PlaintextString = PlaintextString + "E";
-
     
-	// Share results
+	// Share results every 60 seconds
   if(connection.connect(HOST_NAME, HTTP_PORT)) {
     connection.println(HTTP_METHOD + " " + PATH_NAME + "/store/" + PlaintextString + " HTTP/1.1");
     Serial.println(HTTP_METHOD + " " + PATH_NAME + "/store/" + PlaintextString + " HTTP/1.1");
@@ -262,22 +265,44 @@ void loop()
   } else {
     Serial.println("Connection failed");
   }
-    
-      /*
-      delay(100);
-      while(connection.available())
-      {
-        // read an incoming byte from the server and print them to serial monitor:
-        char c = connection.read();
-        Serial.print(c);
-      }
-      */    
+
+  /* // Print debug info: answer from server
+  delay(100);
+  while(connection.available())
+  {
+    // read an incoming byte from the server and print them to serial monitor:
+    char c = connection.read();
+    Serial.print(c);
+  }
+  */    
+
   if(!connection.connected()) connection.stop();
 
-
-  drawText();
-  
   for(k=0;k<60;k++){
+    // Read sensors every second
+    // Read TSL
+    SensorTSL.getEvent(&event);
+    if (event.light) LuxFloat = event.light;
+  
+    if (iaqSensor.run()) {
+      // New data is available 
+      // Convert measurements to string
+      PlaintextString = "S-";
+      PlaintextString = PlaintextString + String(timeClient.getEpochTime())+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.temperature)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.pressure / 100.0)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.humidity)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.iaq)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.staticIaq)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.co2Equivalent)+"-";
+      PlaintextString = PlaintextString + String(iaqSensor.breathVocEquivalent)+"-";
+      PlaintextString = PlaintextString + String(LuxFloat)+"-";
+      PlaintextString = PlaintextString + "E";
+    } else {
+      // Wait for more data
+      checkIaqSensorStatus();
+    }
+    drawText();
     delay(1000);
   }
 }
